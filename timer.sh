c593b90371fb3ab8fb46704209f3b9896f669386
WolfZweiler <61819873+zweilosec@users.noreply.github.com>
2023-11-05 19:49:27 -0600

    Delete timer.sh

--- timer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-printf "[-] Running customization playbooks\n"
-start=$SECONDS            
-sleep 61
-end=$SECONDS 
-runtime=$(( "$end - $start" ))
-mins=$(( "$runtime / 60" ))
-secs=$(( "$runtime % 60" ))
-printf "[+] Deployment finished in $mins mins and $secs seconds.\n"
