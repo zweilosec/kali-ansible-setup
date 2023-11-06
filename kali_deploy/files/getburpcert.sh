f953df0f5459f1ccbebaa0da29f9bb845ba0b9f3
WolfZweiler <61819873+zweilosec@users.noreply.github.com>
2023-11-05 20:33:12 -0600

    add attribution to ippsec for script

--- kali_deploy/files/getburpcert.sh
+++ kali_deploy/files/getburpcert.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Thanks to ippsec for this script!
+# https://github.com/IppSec/parrot-build
 /bin/bash -c "timeout 45 /usr/lib/jvm/java-17-openjdk-amd64/bin/java -Djava.awt.headless=true -jar /usr/share/burpsuite/burpsuite.jar < <(echo y) &" 
 sleep 30
 curl http://localhost:8080/cert -o /tmp/cacert.der
