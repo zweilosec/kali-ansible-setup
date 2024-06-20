ea789036574c0b9ee95e2f54f04a80a5b9ca006c
WolfZweiler <61819873+zweilosec@users.noreply.github.com>
2024-06-20 18:43:16 -0500

    Update java jdk version

--- kali_deploy/files/getburpcert.sh
+++ kali_deploy/files/getburpcert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Thanks to ippsec for this script!
 # https://github.com/IppSec/parrot-build
-/bin/bash -c "timeout 45 /usr/lib/jvm/java-17-openjdk-amd64/bin/java -Djava.awt.headless=true -jar /usr/share/burpsuite/burpsuite.jar < <(echo y) &" 
+/bin/bash -c "timeout 45 /usr/lib/jvm/java-21-openjdk-amd64/bin/java -Djava.awt.headless=true -jar /usr/share/burpsuite/burpsuite.jar < <(echo y) &" 
 sleep 30
 curl http://localhost:8080/cert -o /tmp/cacert.der
 exit
