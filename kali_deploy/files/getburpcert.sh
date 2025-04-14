21ec8e291dcfc628352adf1f4ebdc1f36b32ef30
WolfZweiler <61819873+zweilosec@users.noreply.github.com>
2025-04-14 19:05:13 +0000

    fix #5 #4 #3, temp mitigate #2 #1

--- kali_deploy/files/getburpcert.sh
+++ kali_deploy/files/getburpcert.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Thanks to ippsec for this script!
 # https://github.com/IppSec/parrot-build
-/bin/bash -c "timeout 45 /usr/lib/jvm/java-21-openjdk-amd64/bin/java -Djava.awt.headless=true -jar /usr/share/burpsuite/burpsuite.jar < <(echo y) &" 
+burp=$(find / -name burp*.jar 2>/dev/null | tail -1)
+/bin/bash -c "timeout 45 /usr/bin/java -Djava.awt.headless=true -jar $burp < <(echo y) &"
 sleep 30
 curl http://localhost:8080/cert -o /tmp/cacert.der
 exit
